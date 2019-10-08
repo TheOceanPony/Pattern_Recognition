e1f8a77846d10cdd261762048a58b16526a19e7a
Ocean <TheOceanPony@users.noreply.github.com>
2019-10-08 16:33:42 +0300

    Uploaded existing tasks for further editing

--- Bayesian/Second/SecondModule.py
+++ Bayesian/Second/SecondModule.py
@@ -1,55 +1,55 @@
 def parse(_string):
-	temp = _string.split('\n')
-	temp = temp[1].split(' ')
+    temp = _string.split('\n')
+    temp = temp[1].split(' ')
 
-	for i in range(0, len(temp)):
-		temp[i] = int(temp[i])
-	return temp
+    for i in range(0, len(temp)):
+        temp[i] = int(temp[i])
+    return temp
 
 
 def normalise(_heatmap):
-	total = sum(_heatmap)
+    total = sum(_heatmap)
 
-	for i in range(0, len(_heatmap)):
-		_heatmap[i] = _heatmap[i] / total
+    for i in range(0, len(_heatmap)):
+        _heatmap[i] = _heatmap[i] / total
 
-	return _heatmap
+    return _heatmap
 
 
 def predict_L1(_heatmap):
-	sum = 0
-	for i in range(0, len(_heatmap)):
-		if sum >= 0.5:
-			return i-1
-		sum += _heatmap[i]
-	return "!ERROR! - bad heatmap"
+    total = 0
+    for i in range(0, len(_heatmap)):
+        if total >= 0.5:
+            return i-1
+        total += _heatmap[i]
+    return "!ERROR! - bad heatmap"
 
 
 def predict_delta(_heatmap, _delta):
-	temp = []
-	for i in range(_delta, len(_heatmap) - _delta):
-		temp.append(Isum(_heatmap, i-_delta, i+_delta))
+    temp = []
+    for i in range(_delta, len(_heatmap) - _delta):
+        temp.append(Isum(_heatmap, i-_delta, i+_delta))
 
-	answer = maxInd(temp) + _delta
-	return answer
+    answer = maxInd(temp) + _delta
+    return answer
 
 # TODO find a replacement to this...nonsense
 def Isum(_arr,  _start=0, _end=-1):
-	if _start > _end:
-		return f"Error: _end: {_end} is less that _start: {_start}";
-	total = 0
-	for i in range(_start, _end+1):
-		total += _arr[i]
-	return total
+    if _start > _end:
+        return f"Error: _end: {_end} is less that _start: {_start}";
+    total = 0
+    for i in range(_start, _end+1):
+        total += _arr[i]
+    return total
 
 
 def maxInd(_arr):
-	maximum = _arr[0]
-	max_index = 0
-	for i in range(1, len(_arr)):
-		if maximum < _arr[i]:
-			maximum = _arr[i]
-			max_ind = i
-	return max_ind
+    maximum = _arr[0]
+    max_index = 0
+    for i in range(1, len(_arr)):
+        if maximum < _arr[i]:
+            maximum = _arr[i]
+            max_ind = i
+    return max_ind
 
 
