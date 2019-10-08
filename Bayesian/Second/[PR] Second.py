f0be5b1dca5d1f95f7d5e56beb8f313fbfb5b39e
Ocean <TheOceanPony@users.noreply.github.com>
2019-10-08 16:33:49 +0300

    Uploaded existing tasks for further editing

--- Bayesian/Second/[PR] Second.py
+++ Bayesian/Second/[PR] Second.py
@@ -1,55 +1,51 @@
 import asyncio
 import websockets
-import Ipy as I
+import SecondModule as sm
 
 SessionID = "Coo1Hazcker3011"
 
 width = 100 	# int[2, 1000]
-loss = "L1"	# L1 or int[0, width)
-totalSteps = 10	# int[1, 1 000 000]
-repeats = 3	# int[1, 1000]
+loss = "L1"		# L1 or int[0, width)
+totalSteps = 10		# int[1, 1 000 000]
+repeats = 2		# int[1, 1000]
 
-async def Second():
+async def second():
 	url = "wss://sprs.herokuapp.com/second/" + SessionID
 	async with websockets.connect(url) as websocket:
 
-		#Connecting
-		request = (f"Let's start with {width} {loss} {totalSteps} {repeats}" )
+		# Connecting
+		request = f"Let's start with {width} {loss} {totalSteps} {repeats}"
 		await websocket.send(request)
 		response = await websocket.recv()
 		print(f"\n>Request: {request} \n<Response: {response}")
 
-		#Main cycle
+		# Main cycle
 		for step in range(1, totalSteps + 1):
 
-			request = ("Ready")
+			request = "Ready"
 			await websocket.send(request)
 			response = await websocket.recv()
 			print(f"\n>Request: {request} \n<Response: {response}")
 
-			heatmap = I.Normalise(I.Parse(response))
+			heatmap = sm.normalise(sm.parse(response))
 
-			if(loss == "L1"):
-				answer = str(I.predictL1(heatmap))
+			if loss == "L1":
+				answer = str(sm.predict_L1(heatmap))
 			else:
-				answer = str(I.predictDelta(heatmap, int(loss)))
+				answer = str(sm.predict_delta(heatmap, int(loss)))
 
 			a = answer + " "
 
-
-			request = (f"{step}\n{a*(repeats-1)}{answer}" )
+			request = f"{step}\n{a*(repeats-1)}{answer}"
 			await websocket.send(request)
 			response = await websocket.recv()
 			print(f"\n{request} \n-={response}")
 
-			#input("--==Press any key==--")
-		request = ("Bye")
+		request = "Bye"
 		await websocket.send(request)
 		response = await websocket.recv()
 		print(f"\n{request} \n-={response}")
 
-
-
 		input("--==Press any key==--")
 
-asyncio.get_event_loop().run_until_complete(Second())
+asyncio.get_event_loop().run_until_complete(second())
