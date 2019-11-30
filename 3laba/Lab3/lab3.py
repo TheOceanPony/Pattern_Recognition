760b053e8d056a0b8db586bd6898b59852a02042
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2019-11-30 13:49:13 +0200

    Create lab3.py

---
+++ 3laba/Lab3/lab3.py
@@ -0,0 +1,30 @@
+from functions import *
+import time
+
+NUMBER_LENGTH = 21
+SCALE = 20
+NOISE = 0.3
+HISTOGRAM = [0.15, 0.2, 0.1, 0.05, 0.1, 0.05, 0.1, 0.05, 0.1, 0.1]
+REPEATS = 1
+
+# Upscaling
+for i in range(0, len(y)):
+    y[i] = upscale(y[i], SCALE)
+
+
+j = 0
+start = time.time()
+# Computing
+for i in range(0, REPEATS):
+    k = generator(NUMBER_LENGTH, y, HISTOGRAM)
+    x = noised(k[0], NOISE)
+    res = recursive_strategy(x, HISTOGRAM, y, NUMBER_LENGTH, NOISE)
+
+    if argmax(res) == k[1]:
+        j += 1
+
+    print(f"{i} | {k[2]} | {argmax(res)%3 == 0}")
+
+end = time.time()
+print(f"================== \n Success rate: {j}/{REPEATS} \n Time: {end-start} sec")
+
